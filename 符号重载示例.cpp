d902472be4e8e3f8c70a86f479f4e65a5d0b0aba
xj7152069 <1132060957@qq.com>
2020-03-23 22:14:12 +0800

    update

--- 符号重载示例.cpp
+++ 符号重载示例.cpp
@@ -12,16 +12,37 @@ private:
 public:
 	A();
 	A(_TT n);
+	~A()
+	{cout<<"you delete a A-Object"<<endl;}
+
+	_TT get_a()
+	{
+		return this->a;
+	}
+	
+//对'='重载只能作为成员函数，不能作为友元函数
+//友元函数可以访问所有同类对象的私有成员；
+//而成员函数只能访问自己对象的私有成员，不能访问同类其他对象的私有成员；
+//以下重载实现了不同数据类型的对象间在赋值时的转换；
+//成员函数可使用类的模板，并定义新的模板
+	template <typename U>
+	A<_TT>& operator=(A<U> & obj)
+	{
+		this->a=obj.get_a();
+		return *this;
+	}
+	void display();
 
 //友元函数不属于类的成员，需要另外定义模板
 //一个模板中可包括多个typename
+//以引用的方式传入对象，不会在函数体中创建临时变量
+//若以传值方式传入则在函数中创建临时对象，离开函数时将删除临时对象并调用析构函数。
     template <typename U, typename V>
-	friend A<double> operator+(A<U> obj1, A<V> obj2);
+	friend A<double>& operator+(A<U>& obj1, A<V>& obj2);
     template <typename U, typename V>
-	friend A<double> operator+(A<U> obj, V b);
+	friend A<double>& operator+(A<U>& obj, V& b);
     template <typename U, typename V>
-	friend A<double> operator+(V b, A<U> obj);
-	void display();
+	friend A<double>& operator+(V& b, A<U>& obj);
 };
 
 template <typename _TT>
@@ -37,31 +58,34 @@ A<_TT>::A(_TT n)//构造函数
 }
 
 template <typename U, typename V>
-A<double> operator+(A<U> obj1, A<V> obj2)//重载+号用于对象相加 
+A<double>& operator+(A<U>& obj1, A<V>& obj2)//重载+号用于对象与对象相加 
 {
-	A<double> obj3;
-	obj3.a = double(obj1.a) + double(obj2.a);
-	return obj3;
+	A<double> *obj3;
+	obj3=new A<double>;
+	obj3->a = double(obj1.a) + double(obj2.a);
+	return *obj3;
 }
 
 template <typename U, typename V>
-A<double> operator+(A<U> obj, V b)//重载+号用于对象与数相加
+A<double>& operator+(A<U>& obj, V& b)//重载+号用于对象与数相加
 {
-	A<double> obj3;
-	obj3.a = double(obj.a) + double(b);
-	return obj3;  
+	A<double> *obj3;
+	obj3=new A<double>;
+	obj3->a = double(obj.a) + double(b);
+	return *obj3;  
 }
 
 template <typename U, typename V>
-A<double> operator+(V b, A<U> obj) //注意友元函数的定义格式
+A<double>& operator+(V& b, A<U>& obj) //注意友元函数的定义格式
 {
-	A<double> obj3;
-	obj3.a = double(obj.a) + double(b);
-	return obj3;  
+	A<double> *obj3;
+	obj3=new A<double>;
+	obj3->a = double(obj.a) + double(b);
+	return *obj3;  
 }
 
 template <typename _TT>
-void A<_TT>::display()
+void A<_TT>::display() //成员函数类外定义时要加'::'
 {
 	cout << a << endl;
 }
@@ -69,22 +93,23 @@ void A<_TT>::display()
 int main()
 {
 	A<int> a1(1);
-	A<int> a2(2);
-	A<double> a3, a4, a5; 
-//根据以上重载规则，任何进行赋值操作的'A'对象，都应定义为double型
-//这是由于没有进行'='的重载，对象间无法实现类型转化
-//'='的重载函数不能作为类的友元函数，只能作为成员函数
-//要对'='做重载似乎较复杂，尚未实现。
+	A<double> a2(2.5);
+	A<float> a3, a4, a5; 
+
 	a1.display();
 	a2.display();
 	a3 = a1 + a2;
 	a3.display();
+	a2.~A(); //可以主动调用析构函数
 
-	float m = 3.1415926;
+	double m = 3.1415926;
 	a4 = a1 + m;
 	a4.display();
 	a5 = m + a1;
 	a5.display();
+
+	a3=a1;
+	a3.display();
 
 	return 0;
 }
